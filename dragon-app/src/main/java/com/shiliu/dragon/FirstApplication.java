c3c8aab576871f5956106381a845da1dcce3deed
wangguangshan <guangshanw@163.com>
2021-05-01 09:42:50 +0800

    引入redis

--- dragon-app/src/main/java/com/shiliu/dragon/FirstApplication.java
+++ dragon-app/src/main/java/com/shiliu/dragon/FirstApplication.java
@@ -2,6 +2,7 @@ package com.shiliu.dragon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @RestController
 @EnableSwagger2
+@EnableCaching
 public class FirstApplication {
 
 	public static void main(String[] args) {
